7d328862503bc5f3ac7aadee907e26d9ca5821e1
jayhatha <jay@jayhathaway.com>
2018-05-24 23:35:43 -0700

    added some comments and removed 'shitbrick' debug function, lolol.' '

--- clock.js
+++ clock.js
@@ -1,33 +1,24 @@
-//getting the date
-var date = new Date();
-var hour = date.getHours();
-  if (hour > 12) {
-    hour = hour - 12;
-  }
-var minute = date.getMinutes();
-var second = date.getSeconds();
-console.log(hour + '\:' + minute + '\:' + second);
-//setting the degrees
-var secDegrees = ((second / 60) * 360);
-var minDegrees = ((minute / 60) * 360) + ((second/60)*6);
-var hourDegrees = ((hour / 12) * 360) + ((minute/60)*30);
-// getting the hand elements
+// getting the hand elements and rotating the hands using transform
 
-// rotating the hands using transform
 function minTick() {
-  let date = new Date();
-  var minute = date.getMinutes();
-  var minDegrees = ((minute / 60) * 360) + ((second/60)*6);
-  var mins = document.querySelector("#minute");
-  mins.style.transform = "rotate(" + minDegrees + "deg)";
+  let date = new Date(); //getting the date
+  var minute = date.getMinutes(); //getting the minutes
+  var minDegrees = ((minute / 60) * 360) + ((second/60)*6); // converting minutes to degrees
+  var mins = document.querySelector("#minute"); //selecting the minute hand
+  mins.style.transform = "rotate(" + minDegrees + "deg)"; // rotating hand to correct position
 }
+//same for hours
 function hourTick() {
   let date = new Date();
   hour = date.getHours();
+  if (hour > 12) {
+    hour = hour - 12;
+  }
   hourDegrees = ((hour / 12) * 360) + ((minute/60)*30);
   var hours = document.querySelector("#hour");
   hours.style.transform = "rotate(" + hourDegrees + "deg)";
 }
+// same for seconds
 function secTick() {
   let date = new Date();
   second = date.getSeconds();
@@ -35,13 +26,13 @@ function secTick() {
   var secs = document.querySelector("#second");
   secs.style.transform = "rotate(" + secDegrees + "deg)";
 }
-function shitBrick() {
-  console.log('shit a brick');
-}
-//setting initial hand positions
+//keeping the clock moving
+
 setInterval(secTick, 500);
 setInterval(minTick, 10000);
 setInterval(hourTick, 20000);
+
+//setting initial hand positions
 hourTick();
 minTick();
 secTick();
