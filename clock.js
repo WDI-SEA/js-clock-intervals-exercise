7d2e77dfc5b88aaddd2a918be0099b8a6139df21
Scott Ammon <scott.o.ammon@gmail.com>
2018-05-25 13:26:56 -0700

    fixed html script tag, css selectors, and js function names

--- clock.js
+++ clock.js
@@ -1,26 +1,26 @@
-window.setInterval(advanceClock, 1000);
+document.addEventListener("DOMContentLoaded", function() {
 
-var handRotation = function() {
-  // create date object with current time
-  var today = new Date;
+var rotateHands = function() {
+  // date object with current time
+  var currentTime = new Date;
   
   // create object that stores second, minute, and hour
   handPositions = {
-    second: (today.getSeconds() / 60) * 360,
-    minute: 6 * (today.getMinutes() + (today.getSeconds() / 60)),
-    hour: (60 * today.getHours() + today.getMinutes()) * 0.5
+    second: 6 * currentTime.getSeconds(),
+    minute: 6 * (currentTime.getMinutes() + (currentTime.getSeconds() / 60)),
+    hour: (60 * currentTime.getHours() + currentTime.getMinutes()) * 0.5
   };
   return handPositions;
 };
 
-// select all hands - but not the background image
 var clockHands = document.getElementsByClassName("hand");
 
-// advance the hands 
 var advanceClock = function () {
-  var handPositions = handRotation();
+  var handPositions = rotateHands();
   // iterate over the three hands and rotate each one based on the handPositions object
   for (var i = 0; i < clockHands.length; i++) {
     clockHands[i].style.transform = "rotate(" + handPositions[clockHands[i].id] + "deg)";
   };
 };
+window.setInterval(advanceClock, 1000);
+});
