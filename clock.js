d0c4b9ffebfd2ec477c389a77b1a7f43b2c7cb7b
jayhatha <jay@jayhathaway.com>
2018-05-25 09:20:34 -0700

    feat drake

--- clock.js
+++ clock.js
@@ -1,13 +1,22 @@
-// getting the hand elements and rotating the hands using transform
+//getting the date
+var date = new Date();
+var hour = date.getHours();
+var minute = date.getMinutes();
+var second = date.getSeconds();
 
+//setting the degrees
+var secDegrees = ((second / 60) * 360);
+var minDegrees = ((minute / 60) * 360) + ((second/60)*6);
+var hourDegrees = ((hour / 12) * 360) + ((minute/60)*30);
+
+// getting the hand elements and rotating the hands using transform
 function minTick() {
-  let date = new Date(); //getting the date
-  var minute = date.getMinutes(); //getting the minutes
-  var minDegrees = ((minute / 60) * 360) + ((second/60)*6); // converting minutes to degrees
-  var mins = document.querySelector("#minute"); //selecting the minute hand
-  mins.style.transform = "rotate(" + minDegrees + "deg)"; // rotating hand to correct position
+  let date = new Date();
+  var minute = date.getMinutes();
+  var minDegrees = ((minute / 60) * 360) + ((second/60)*6);
+  var minuteHand = document.querySelector("#minute");
+  minuteHand.style.transform = "rotate(" + minDegrees + "deg)";
 }
-//same for hours
 function hourTick() {
   let date = new Date();
   hour = date.getHours();
@@ -15,16 +24,15 @@ function hourTick() {
     hour = hour - 12;
   }
   hourDegrees = ((hour / 12) * 360) + ((minute/60)*30);
-  var hours = document.querySelector("#hour");
-  hours.style.transform = "rotate(" + hourDegrees + "deg)";
+  var hourHand = document.querySelector("#hour");
+  hourHand.style.transform = "rotate(" + hourDegrees + "deg)";
 }
-// same for seconds
 function secTick() {
   let date = new Date();
   second = date.getSeconds();
   secDegrees = ((second / 60) * 360);
-  var secs = document.querySelector("#second");
-  secs.style.transform = "rotate(" + secDegrees + "deg)";
+  var secondHand = document.querySelector("#second");
+  secondHand.style.transform = "rotate(" + secDegrees + "deg)";
 }
 //keeping the clock moving
 
