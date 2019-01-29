414a69b7956003dd06792ee55745b206df447847
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-01-29 08:27:41 -0800

    js clock intervals - Sang Choi

---
+++ clock.js
@@ -0,0 +1,30 @@
+
+// Clock needs to have second, minute, hour to tick
+//
+
+var seconds = document.getElementById("second");
+var minutes = document.getElementById("minute");
+var hours = document.getElementById("hour");
+
+
+var sdegrees = 0;
+var mdegrees = 0;
+var hdegrees = 0;
+
+var sdegrees = 0;
+setInterval(function() {
+    seconds.style.transform = "rotate(" + sdegrees + "deg)";
+    sdegrees = sdegrees + 6;
+} ,1000);
+
+var mdegrees = 0;
+setInterval(function() {
+    minutes.style.transform = "rotate(" + mdegrees + "deg)";
+    mdegrees = mdegrees + 6;
+} ,60000);
+
+var hdegrees = 0;
+setInterval(function() {
+    hours.style.transform = "rotate(" + hdegrees + "deg)";
+    hdegrees = hdegrees + 30;
+} ,3600000);
