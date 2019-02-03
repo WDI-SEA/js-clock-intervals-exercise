07934b905f92d10b3b45ecd42e5804e350943d73
mickeychcg <mcowanseattle@gmail.com>
2019-02-02 18:48:46 -0800

    refactored to add smoother hour and second hand movement

--- clock.js
+++ clock.js
@@ -1,15 +1,38 @@
-// Stack PNGs of clock face, hour hand, minute hand, and second hand
-// Create JS DOM functions to update the clock's hands from browser time
+// REFACTORED Clock: with hour hand changing .5deg/minute
+// and smooth sweep second hand
+// used document.onLoad to start the clock - perhaps poor practice?
+// put DOM and time variables in arrays and called the DOM array
+// (clockHands) in the DOM update statements by each array member
+// CSS rotation results in strange resetting behavior
+// due to time set in seconds vs. milleseconds
 
-// DOM Variable for each hand
-var hour = document.getElementById("hour");
-var minute = document.getElementById("minute");
-var second = document.getElementById("second");
+document.onLoad = browserClock();
 
-// CLOCK CODE WITHOUT CURRENT TIME
+function browserClock() {
+    clock();
+    setInterval(clock, 1000);
+}
+function clock() {
+    var date = new Date();
+    var time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    var clockHands = [document.getElementById("hour"), document.getElementById("minute"), document.getElementById("second")];
+    var hour = (time[1] / 2) + (time[0] * 30);
+
+
+    clockHands[0].style.transform = ("rotate(" + hour + "deg)");
+    clockHands[1].style.transform = ("rotate(" + time[1] * 6 + "deg)");
+    clockHands[2].style.transform = ("rotate(" + time[2] * 6 + "deg)");
+}
+
+// ORIGINAL CLOCK CODE WITHOUT CURRENT TIME
+// This is a mess, but it's better than
+// the below example that uses current time
+
+// var hour = document.getElementById("hour");
+// var minute = document.getElementById("minute");
+// var second = document.getElementById("second");
 
 // Variables set to ) for seconds, minutes & hours
-
 // var sec = 0;
 // var min = 0;
 // var hr = 0;
@@ -51,46 +74,34 @@ var second = document.getElementById("second");
 //     hr.style.transform = ("rotate(" + hr / 12 * 360 + "deg)")
 // }, 3600000)
 
-// Bonus, make it work with Current Time
 // CLOCK CODE WITH CURRENT TIME
+// clumsy implementation
+// hour hand jumps to new hour
 
-// REFACTOR: make the below locally scoped
-// ADD: move hour hand 0.5deg per minute
+// var secToDegrees = function (sec) {
+//     var secDegrees = (currentSec / 60) * 360;
+//     return secDegrees;
+//     console.log(secToDegrees);
+// }
+// var MinToDegrees = function (Min) {
+//     var MinDegrees = (currentMin / 60) * 360;
+//     return MinDegrees;
+// }
+// var hrsToDegrees = function (hrs) {
+//     var hrsToDegrees = (30 * currentHr % 12) + currentMin / 2;
+//     return hrsToDegrees;
+//     // var hrsToDegrees = Math.abs(hrsToDegrees - secOrMinDegrees);    
+//     // return hrsToDegrees - Math.min(360 - hrsToDegrees, hrsToDegrees);
+// }
 
-var timer = new Date();
-var currentSec = timer.getSeconds();
-var currentMin = timer.getMinutes();
-var currentHr = timer.getHours();
+// var fullClockTick = setInterval(function () {
+//     timer = new Date();
+//     currentSec = timer.getSeconds();
+//     currentMin = timer.getMinutes();
+//     currentHr = timer.getHours();
+// });
 
-var fullClockTick = setInterval(function () {
-    // var interval = clearInterval ();
-    timer = new Date();
-    currentSec = timer.getSeconds();
-    currentMin = timer.getMinutes();
-    currentHr = timer.getHours();
-    ticker();
-}, 1000);
-
-var secMinToDegrees = function (secOrMin) {
-    var secOrMinDegrees = (secOrMin / 60) * 360;
-    return secOrMinDegrees;
-}
-
-var hrsToDegrees = function (hrs) {
-    var hrsToDegrees = (hrs / 12) * 360;
-    return hrsToDegrees;
-    // var hrsToDegrees = Math.abs(hrsToDegrees - secOrMinDegrees);    
-    // return hrsToDegrees - Math.min(360 - hrsToDegrees, hrsToDegrees);
-}
-
-var ticker = function () {
-    second.style.transform = ("rotate(" + secMinToDegrees(currentSec) + "deg)");
-    minute.style.transform = ("rotate(" + secMinToDegrees(currentMin) + "deg)");
-    hour.style.transform = ("rotate(" + hrsToDegrees(currentHr) + "deg)");
-}
-
-
-//Hour hand formulas
-// def clockangles(hour, minute):
-// ans = abs((hour * 30 + minute * 0.5) - (minute * 6))
-// return min(360 - ans, ans)
+// var ticker = function () {
+// setInterval(ticker, 1000);
+// }
+// ticker();
