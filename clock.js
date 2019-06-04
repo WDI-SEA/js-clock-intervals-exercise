494a69986813bfa18dce501bb02b8e1d767fed69
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-06-04 09:21:23 -0700

    adjustments

--- clock.js
+++ clock.js
@@ -2,29 +2,18 @@
 var secondHand = document.getElementById("second");
 var minuteHand = document.getElementById("minute");
 var hourHand = document.getElementById("hour");
-//var degrees = 45;
 
+//for clock to display real time:
 //var interval = setInterval(runClock, 1000);
 
-//function runClock() {
-  //var degrees = 1;
   
   var second = 1;
   var minute = 1;
   var hour = 1;
 
  setInterval(secondRotation, 1000);
- setInterval(minuteRotation, 6000);
- setInterval(hourRotation, 36000);
-
-  
-  
-  /*var now = new Date();
-
-  var second = now.getSeconds();
-  var minute = now.getMinutes();
-  var hour = now.getHours();
-  */
+ setInterval(minuteRotation, 600000);
+ setInterval(hourRotation, 3600000);
   
   function secondRotation() {
     var secRotation = second * 6; // --> seconds * 360degrees/60 seconds
@@ -43,30 +32,28 @@ var hourHand = document.getElementById("hour");
     hourHand.style.transform = "rotate(" + hrRotation + "deg)";
     hour++;
   }
-    /*
-    var now = new Date();
 
-var second = now.getSeconds();
-var minute = now.getMinutes();
-var hour = now.getHours();
-console.log(hour + minute + second);
-  }
-  */
+//function for real time:
+/*   
+function runClock() {
+  var now = new Date();
+
+  var second = now.getSeconds();
+  var minute = now.getMinutes();
+  var hour = now.getHours();
   
-  /*
+  
   //convert seconds, minutes and hours to degrees:
   var secRotation = second * 6; // --> seconds * 360degrees/60 seconds
   var minuteRotation = (minute * 6) + (second * 0.1);// --> minutes * 360/degrees/60minutes) + (seconds * (360deg/60minutes)* (1 minute/60 seconds)
   var hourRotation = (hour * 30) + (minute * 0.5);//--> hours * 360deg/12hours) + (minutes * 360deg/12hours)*(1 hour/60min)
-  */
   
-  /*
   secondHand.style.transform = "rotate(" + secondRotation + "deg)";
   minuteHand.style.transform = "rotate(" + minuteRotation + "deg)";
   hourHand.style.transform = "rotate(" + hourRotation + "deg)";
-  */
   
-//};
+}; 
+*/
 
 
 
