ebff7b8658594a18028426f5506530ff564d16b3
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-06-04 09:56:37 -0700

    cosmetic updates

--- clock.js
+++ clock.js
@@ -2,58 +2,55 @@
 var secondHand = document.getElementById("second");
 var minuteHand = document.getElementById("minute");
 var hourHand = document.getElementById("hour");
-
-//for clock to display real time:
-//var interval = setInterval(runClock, 1000);
-
   
-  var second = 1;
-  var minute = 1;
-  var hour = 1;
+//for ticking clock:
+/*
+var second = 1;
+var minute = 1;
+var hour = 1;
 
- setInterval(secondRotation, 1000);
- setInterval(minuteRotation, 600000);
- setInterval(hourRotation, 3600000);
+setInterval(secondRotate, 100);
+setInterval(minuteRotate, 1000);
+setInterval(hourRotate, 6000);
   
-  function secondRotation() {
+function secondRotate() {
     var secRotation = second * 6; // --> seconds * 360degrees/60 seconds
     secondHand.style.transform = "rotate(" + secRotation + "deg)";
     second++;
-  }
-
-  function minuteRotation() {
+}
+function minuteRotate() {
     var minRotation = (minute * 6) + (second * 0.1);// --> minutes * 360/degrees/60minutes) + (seconds * (360deg/60minutes)* (1 minute/60 seconds)
     minuteHand.style.transform = "rotate(" + minRotation + "deg)";
     minute++;
-  }
-
-  function hourRotation() {
+}
+function hourRotate() {
     var hrRotation = (hour * 30) + (minute * 0.5);//--> hours * 360deg/12hours) + (minutes * 360deg/12hours)*(1 hour/60min)
     hourHand.style.transform = "rotate(" + hrRotation + "deg)";
     hour++;
-  }
-
-//function for real time:
-/*   
-function runClock() {
-  var now = new Date();
-
-  var second = now.getSeconds();
-  var minute = now.getMinutes();
-  var hour = now.getHours();
-  
-  
-  //convert seconds, minutes and hours to degrees:
-  var secRotation = second * 6; // --> seconds * 360degrees/60 seconds
-  var minuteRotation = (minute * 6) + (second * 0.1);// --> minutes * 360/degrees/60minutes) + (seconds * (360deg/60minutes)* (1 minute/60 seconds)
-  var hourRotation = (hour * 30) + (minute * 0.5);//--> hours * 360deg/12hours) + (minutes * 360deg/12hours)*(1 hour/60min)
-  
-  secondHand.style.transform = "rotate(" + secondRotation + "deg)";
-  minuteHand.style.transform = "rotate(" + minuteRotation + "deg)";
-  hourHand.style.transform = "rotate(" + hourRotation + "deg)";
-  
-}; 
+}
 */
+
+//for clock to display real time:
+function runClock() {
+    var now = new Date();
+
+    var second = now.getSeconds();
+    var minute = now.getMinutes();
+    var hour = now.getHours();
+  
+    //convert seconds, minutes and hours to degrees:
+    var secondRotation = second * 6; // --> seconds * 360degrees/60 seconds
+    var minuteRotation = (minute * 6) + (second * 0.1);// --> minutes * 360/degrees/60minutes) + (seconds * (360deg/60minutes)* (1 minute/60 seconds)
+    var hourRotation = (hour * 30) + (minute * 0.5);//--> hours * 360deg/12hours) + (minutes * 360deg/12hours)*(1 hour/60min)
+  
+    //rotate second, minute, and hour hands:
+    secondHand.style.transform = "rotate(" + secondRotation + "deg)";
+    minuteHand.style.transform = "rotate(" + minuteRotation + "deg)";
+    hourHand.style.transform = "rotate(" + hourRotation + "deg)";
+}; 
+var interval = setInterval(runClock, 1000);
+
+
 
 
 
