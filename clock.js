73e5bb47e89765480c5c2d1816eb48fc1e6e14c1
Kelsey Cox <kelseycox@Lucy.local>
2019-06-04 09:24:00 -0700

    created clock

--- clock.js
+++ clock.js
@@ -10,8 +10,8 @@ function secondRotation() {
     seconds = seconds + 1;
     var degrees = (seconds/60) * 360;
     secondHand.style.transform = "rotate(" + degrees + "deg)";
-    if (seconds === 60) {
-        seconds = 0;
+    if (seconds === 61) {
+        seconds = 1;
     }
 };
 
@@ -19,8 +19,8 @@ function minuteRotation () {
     minutes = minutes + 1;
     var degrees = (minutes/60) * 360;
     minuteHand.style.transform = "rotate(" + degrees + "deg)";
-    if (minutes === 60) {
-        minutes = 0;
+    if (minutes === 61) {
+        minutes = 1;
     }
 };
 
@@ -28,11 +28,11 @@ function hourRotation () {
     hours = hours + 1
     var degrees = (hours/12) * 360;
     hourHand.style.transform = "rotate(" + degrees + "deg)";
-    if (hours === 12) {
-        hours = 0;
+    if (hours === 13) {
+        hours = 1;
     }
 };
 
-setInterval(secondRotation, 10);
-setInterval(minuteRotation, 600);
-setInterval(hourRotation, 36000);
+setInterval(secondRotation, 1000);
+setInterval(minuteRotation, 60000);
+setInterval(hourRotation, 3600000);
