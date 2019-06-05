f1c135ae4be40290f8de8ee54cb28a64e058a4af
Vincent Renaldo Jita <daejaeblaq@Vincents-MacBook-Pro.local>
2019-06-04 17:13:48 -0700

    done

--- clock.js
+++ clock.js
@@ -10,9 +10,10 @@ setInterval(secondRotation, 1000);
 setInterval(minuteRotation, 60000);
 
 setInterval(hourRotation, 3600000);
-
+//doing 3 intervals will get out of sync even faster. nothing happens at same time in js.
 
 //Additional Functions
+//declare function outside so that its global
 function secondRotation() {
     var degrees = (e / 60) * 360
     second.style.transform = "rotate(" + degrees + "deg)";
@@ -30,3 +31,31 @@ function hourRotation() {
     hour.style.transform = "rotate(" + degrees + "deg)";
     e++;
 }
+    
+//Date
+function windUpClock() {
+    var date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+}
+
+//TEACHERS ANSWER
+// document.addEventListener('DOMContent', function() {
+//     var now = new Date();
+//     //library called moment that makes date easier
+
+//     minutes = now.getMinutes();
+//     seconds = now.getSeconds();
+//     hours = now.getHours() >=12 ? now.getHiyrs() - 12 : now.getHours();
+
+//     sHand = document.getElementById("second");
+//     mHand = document.getElementById("minute");
+//     hHand = document.getElementById("hour");
+
+//     mBox = document.getElementById('monthbox');
+//     dBox = document.getElementById('daybox');
+//     yBox = document.getElementById('yearbox');
+
+// });
+
