ff650d486306e20aeadfa0e256bba515b20e35ea
Guelito1 <mflorescacravens@gmail.com>
2019-06-06 13:49:08 -0700

    clock commit

--- clock.js
+++ clock.js
@@ -8,20 +8,29 @@ var secondHand = document.getElementById('second');
 
 setInterval(setClock, 1000);
 
-function setClock() {
-    var time = new Date();
-    var seconds = time.getSeconds() / 60;
-    var minutes = (seconds + time.getMinutes()) / 60;
-    var hours = (minutes + time.getHours()) / 12;
-    
-    setRotation(secondHand, seconds);
-    setRotation(minuteHand, minutes);
-    setRotation(hourHand, hours);
+    function setClock() {
+        var time = new Date();
+        var seconds = time.getSeconds() / 60;
+        var minutes = (seconds + time.getMinutes()) / 60;
+        var hours = (minutes + time.getHours()) / 12;
+        
+        setRotation(secondHand, seconds);
+        setRotation(minuteHand, minutes);
+        setRotation(hourHand, hours);
 }
 
 function setRotation(element, rotationRatio) {
     element.style.setProperty('transform', "rotate("+ rotationRatio * 360 +"deg)")
 }
+
+
+
+// what do you need to do to get a clock to do the "bounce back" motion
+
+// the second hand needs to overshoot by a slight
+//  degree and then come back to...
+
+
 
 // hour = hour.setInterval(function tickTock() {
 
