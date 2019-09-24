e4b55ba5c3c0b6b2c0ebf92c64ca1ba6768f2f55
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-09-23 19:44:12 -0700

    finished

--- clock.js
+++ clock.js
@@ -6,8 +6,8 @@ console.log(second)
 console.log(minute)
 console.log(hour)
 s = 1;
-h = 1;
 m = 1;
+h = 1;
 
 let secondsMove = function() {
     var Secondsdegrees = (1/60) * 360 * s++;
@@ -16,3 +16,19 @@ let secondsMove = function() {
 }
 
 setInterval(secondsMove, 1000);
+
+let minutesMove = function() {
+    let Minutedegrees = (1/60) * 360 * m++;
+    minute.style.transform = "rotate(" + Minutedegrees + "deg)"; 
+    console.log(Minutedegrees);
+}
+
+setInterval(minutesMove, 60000);
+
+let hourMove = function() {
+    let Hourdegrees = (5/60) * 360 * h++;
+    hour.style.transform = "rotate(" + Hourdegrees + "deg)"; 
+    console.log(Hourdegrees);
+}
+
+setInterval(hourMove, 360000);
