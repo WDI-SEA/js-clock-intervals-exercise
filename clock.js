67b93ea6298a8e5ec86bd4b72a76efd86e0be718
palo89 <alison_lo89@yahoo.com>
2020-12-17 21:54:41 -0700

    initial try, added css, started javascript

---
+++ clock.js
@@ -0,0 +1,25 @@
+// need an seconds function
+// need an minutes function
+// need an hours function
+// will need to get each function to rotate
+// set the clock
+console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', () => {
+console.log("DOM LOADED")
+}
+
+let degreeSeconds = 6
+let degreeMinutes = 6
+let degreeHours = 30
+
+function clock () {
+    var now = new Date;
+    var seconds = date.degreeSeconds ();
+    var minutes = date.degreeMinutes ();
+    var hours = date.degreeHours ();
+};
+
+let (var i = 0; i < hands.length; i++) {
+    var elements = document.querySelectorAll('.' hands[i].hand);
+    
+}
