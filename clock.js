be227ad96c9c384c50af7f83ec44e95e4afcaff3
edgard Jara <edgardjara@edgards-MacBook-Pro.local>
2020-12-18 09:03:19 -0800

    First clock

--- clock.js
+++ clock.js
@@ -1,1 +1,18 @@
-console.log("hello git")
+
+var seconds = document.getElementById("second")
+var minutes = document.getElementById("minute")
+var hours = document.getElementById("hour")
+
+let secondCount=6
+let minuteCount=.1
+let hourCount=.00833333
+let sRotation=0
+let mRotation=0
+let hRotation=0
+
+setInterval(function() {
+    seconds.style.transform = "rotate(" + (sRotation += secondCount) + "deg)"
+    minutes.style.transform = "rotate(" + (mRotation += minuteCount) +"deg)"
+    hours.style.transform = "rotate(" + (hRotation += hourCount) + "deg)"
+
+}, 1000)
