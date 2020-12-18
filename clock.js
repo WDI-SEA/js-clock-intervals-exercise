2c928d8a31501b7f5532b7d90a2ea6363b478eba
Vince Hintermeister <vv.hintermeister@gmail.com>
2020-12-18 10:10:00 -0600

    js-clock-intervals-exercise

--- clock.js
+++ clock.js
@@ -4,7 +4,7 @@
 //el.style.transform = "rotate(" + degrees + "deg)";
 //
 //secondRotation(0); // returns 0
-///secondRotation(59); // returns (59 / 60) * 360
+//secondRotation(59); // returns (59 / 60) * 360
 //
 //hourRotation(12); // returns 0
 //hourRotation(5);
@@ -13,8 +13,16 @@ var secondsHand = document.getElementById("second");
 var minutesHand = document.getElementById('minute');
 var hoursHand = document.getElementById('hour');
 
+let secondCount = 6;
+let minuteCount = .1;
+let hourCount = .00833333;
+
+let secondRotation = 0;
+let minuteRotation = 0;
+let hourRotation = 0;
+
 setInterval(function() {
-    minutesHand.style.transform = "rotate(90deg)";
-    hoursHand.style.transform = "rotate(90deg)"; 
-    secondsHand.style.transform = "rotate(90deg)";
+    secondsHand.style.transform = "rotate(" + (secondRotation += secondCount) + "deg)";
+    minutesHand.style.transform = "rotate(" + (minuteRotation += minuteCount) + "deg)"; 
+    hoursHand.style.transform = "rotate(" + (hourRotation += hourCount) + "deg)";
 },1000)
