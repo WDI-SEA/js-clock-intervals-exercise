3e9abc024c722237b37fb7597aa31b0b9dca2aa7
Your Name <you@example.com>
2020-12-17 21:17:45 -0800

    Turns out I was incrementing by 60 seconds, and not 1 second, every 1000ms. It was causing the clock to jump so much that it looked like things were ticking as normal but with the wrong hands...

--- clock.js
+++ clock.js
@@ -6,24 +6,24 @@ let minutes = 0;
 let hours = 0;
 
 function tickSecondHand(seconds) {
-    let degreeRotation = 360/60 * seconds; // 360 degrees in a circle, 60 seconds in a rotation.
-    second.style.transform = 'rotate(' + degreeRotation + 'deg)'; // CSS is written as transform: rotate(ndeg): https://usefulangle.com/post/300/javascript-image-rotate
+    let secondDegreeRotation = 360/60 * seconds; // 360 degrees in a circle, 60 seconds in a rotation.
+    second.style.transform = 'rotate(' + secondDegreeRotation + 'deg)'; // CSS is written as transform: rotate(ndeg): https://usefulangle.com/post/300/javascript-image-rotate
 }
 
 function tickMinuteHand(seconds) {
     let minutes = seconds/60; // 60 seconds in a minute.
-    let degreeRotation = 360 / 60 * minutes; // 360 degrees in a circle, 60 minutes in a rotation.
-    minute.style.transform = 'rotate(' + degreeRotation + 'deg)';
+    let minuteDegreeRotation = 360 / 60 * minutes; // 360 degrees in a circle, 60 minutes in a rotation.
+    minute.style.transform = 'rotate(' + minuteDegreeRotation + 'deg)';
 }
 
 function tickHourHand(seconds) {
     let hours = seconds/3600; // 3600 seconds in an hour.
-    let degreeRotation = 360 / 12 * hours; // 360 degrees in a circle, 12 hours in a rotation.
-    hour.style.transform = 'rotate(' + degreeRotation + 'deg)';
+    let hourDegreeRotation = 360 / 12 * hours; // 360 degrees in a circle, 12 hours in a rotation.
+    hour.style.transform = 'rotate(' + hourDegreeRotation + 'deg)';
 }
 
 function tick() {
-    secs += 60;
+    secs += 1;
     tickSecondHand(secs);
     tickMinuteHand(secs);
     tickHourHand(secs);
