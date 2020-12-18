9193f1ed6ffab9965aa7f2a99353a1335d1fc36a
Morgan Harris <nonsensemh1@gmail.com>
2020-12-18 08:33:20 -0800

    attempting to send

---
+++ clock.js
@@ -0,0 +1,163 @@
+// const degrees = 45;
+// const hourhand = document.getElemenyById('#hour');
+// const minutehand = document.getElemenyById('#minute');
+// const secondhand = document.getElemenyById('#second');
+
+
+// hourhand.style.transform = "rotate(" + degrees + "deg)";
+
+// 10 attempts over 5.5 hours
+
+// hourH = document.getElementyById('#hour').onload;
+// minuteH = document.getElementyById('#minute').onload;
+// secondH = document.getElementyById('#second').onload;
+
+// }
+// const minuteHand = () => {
+//   minute = document.getElemenyById('#minute');minute.style.transform = "rotate(" + degrees + "deg";}
+
+// const secondHand = () => {
+// second = document.getElemenyById('#second');second.style.transform = "rotate(" + degrees + "deg)";
+// }
+
+// // GET ELEMENTS AND SET OBJECTS, PROMPT WAS WRONG
+// // NOW CALL BACKS TO ABOVE BELOW
+
+// hourHand()
+
+// function hours() {
+// 
+// hourHand.style.transform = "rotate(" + degrees + "deg)"
+// }
+
+// hours()
+// // // function appending hourHand element
+
+// const moveMinute = () => {
+// 
+// minuteHand.style.transform = "rotate(" + degrees + "deg)" 
+// }
+
+// // // // // function appending minuteHand element
+
+// const moveSecond = () => {
+// var secondHand = document.getElemenyById('#second')
+// secondHand.style.transform = "rotate(" + degrees + "deg)" 
+// }
+// // // function appending secondHand element
+
+// const hourRotate = (moveHour) => {
+// moveHour()
+// conversion()
+// }
+
+// STACK CALLBACK FUNCTION MOVE HOUR
+
+
+//  GOT AND SET CONTENT ABOVE, NEED TO ADD EFFECTS BELOW
+
+// // CALLBACK FUNCTIONS NEXT STEP?
+// // function hourConvert(x) {
+// //      return ((x/12) * 360)
+// }
+// // function minuteConvert(y) {
+// //   return ((y/60) * 360)
+// //   console.log(return)
+// // }
+// // function secondConvert(z) {
+// //   return ((z/60) * 360)
+// //   console.log(return)
+// // }
+
+// // console.log('face')
+
+
+
+// // const hourRotate = () => {
+// //   x = 
+// //   return ( (value/12) * 360   )
+// // }
+
+// // function hourHandMove (x) {
+// // const hourhand = document.getElemenyById('#hour');
+// // hourhand.style.transform = "rotate(" + degrees + "deg)";
+// // }
+
+// // function hourCircle()
+
+
+
+
+
+
+
+
+// // const minutehand = document.getElemenyById('#minute');
+// // minutehand.style.transform = "rotate(" + degrees + "deg)";
+
+// // const secondhand = document.getElemenyById('#second');
+// // secondhand.style.transform = "rotate(" + degrees + "deg)";
+
+// // function degreeConvert(x,y) {
+// //   return (x/y) * 360
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+// // function hourRotation(x) {
+// //   if(x === 12){
+// //     return 0
+// //   }
+// //   else {
+// //     return degreeConvert(x,12)
+// //     console.log(result)
+// //   }
+// // }
+// // hourRotation(5)
+
+
+// // console.log(degreeConvert(5,12))
+// // function hourRotation(x) {
+// // if (x === 12) {
+// //   return 0
+// // }
+// // else {
+// //   return ((x/12) * 360)
+// // }
+// // }
+// // hourRotation(0)
+
+
+
+// // const hourRotation = {
+// //   if (x !== 12) {
+// //     return (x)
+// //   }
+// //   return( (x)/6 * 360)
+// // }; 
+
+// // const minuteRotation(y) = {
+// //   return( (y/60) * 360)
+// // }; 
+
+// // const secondRotation = () => {
+// //   return( (x/60) * 360)
+// // }; 
+
+
+// // secondRotation(59); // returns (59 / 60) * 360
+
+// // hourRotation(12); // returns 0
+// // hourRotation(5); // returns (5 / 12) * 360
+
+
+
