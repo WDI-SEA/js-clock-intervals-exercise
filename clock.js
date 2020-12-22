28eef171acd6139df97a663dd9ff2c486656262a
jaystar007 <jstarks0@Hotmail.com>
2020-12-21 21:38:00 -0800

    Real Clock Homework Finished

---
+++ clock.js
@@ -0,0 +1,24 @@
+let secondDegree = 6;
+let minuteDegree = 6;
+let hourDegree = 30;
+
+const secondRotation = () => {
+  let second = document.getElementById('second');
+  second.style.transform = 'rotate('+secondDegree+ 'deg)';
+  secondDegree += 6;
+}
+setInterval(secondRotation, 1000);
+
+const minuteRotation = () => {
+  let minute = document.getElementById('minute');
+  minute.style.transform = 'rotate('+minuteDegree+ 'deg)';
+  minuteDegree += 6;
+}
+setInterval(minuteRotation, 60000);
+
+const hourRotation = () => {
+  let hour = document.getElementById('hour');
+  minute.style.stransform = 'rotate('+hourDegree+ 'deg)';
+  hourDegree +=6;
+  
+}
