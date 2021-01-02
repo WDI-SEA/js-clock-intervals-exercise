103c901856c1f4900886b53aab6dcfd9813f8bc4
FrankieRG <garciaf90@gmail.com>
2021-01-02 14:52:34 -0800

    ClockHomework

---
+++ clock.js
@@ -0,0 +1,27 @@
+const hourHand = document.querySelector('.hour');
+    const minuteHand = document.querySelector('.minute');
+    const secondHand = document.querySelector('.seconds');
+
+
+    function getTime() {
+        const now = new Date();
+
+        const seconds = now.getSeconds();
+        const secondsDegree = (((seconds / 60) * 360) + 90);
+        secondHand.style.transform = `rotate(${secondsDegree}deg)`
+
+
+        const minutes = now.getMinutes();
+        const minutesDegree = (((minutes / 60) * 360) + 90);
+        minuteHand.style.transform = `rotate(${minutesDegree}deg)`
+
+
+        const hours = now.getHours();
+        const hoursDegree = (((hours / 60) * 360) + 90);
+        hourHand.style.transform = `rotate(${hoursDegree}deg)`
+
+
+
+    }
+
+    setInterval(getTime, 1000);
