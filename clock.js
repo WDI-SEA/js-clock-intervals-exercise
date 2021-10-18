2c56db8ecb7dea6bebc9bc2039674a723cbe2804
paulinal3 <paulinal3@outlook.com>
2021-10-18 19:21:26 -0400

    clock image aligned

---
+++ clock.js
@@ -0,0 +1,6 @@
+// what html elements will be manipulated?
+const hourHand = document.getElementById('hour')
+const minuteHand = document.getElementById('minute')
+const secondHand = document.getElementById('second')
+// create intervals to make the clock tick.
+// hour hand will run 
