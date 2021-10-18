04dc8807a1153e05caf651004dfc7bc55ec3cd4e
ornery-mouse <cory.sorel@gmail.com>
2021-10-18 16:49:01 -0500

    initial commit - clock lined up, JS begun

---
+++ clock.js
@@ -0,0 +1,22 @@
+//set interval for hour
+//set interval for minute
+//set interval for second
+
+//move hands
+//select hour hand
+const hourHand = document.getElementById('hour')
+//adjust CSS
+//select minute hand
+const minuteHand = document.getElementById('minute')
+//adjust CSS
+//select second hand
+const secondHand = document.getElementById('second')
+//adjust CSS
+//do math for interval to degree
+const secondConversion = (s) => {
+    return (s/60) * 360
+}
+
+const moveHour = () => {
+    
+}
