3fc83e62c04f5be118b5339727a5e78da390b864
harrdev <jasonharrphx@live.com>
2021-10-18 16:05:07 -0700

    finished, need to play change parameters and see if this is the best way

--- clock.js
+++ clock.js
@@ -1,28 +1,32 @@
 const second = document.getElementById("second");
 const minute = document.getElementById("minute");
-const hour = document.getElementById("hour)");
+const hour = document.getElementById("hour");
+const now = new Date();
 
+// Setting recursive function for rotation seconds by grabbing current time and transforming the second hand to the correct degree
 const secRotation = () => {
     let now = new Date();
     let seconds = now.getSeconds();
     second.style.transform = 'rotate(' + (seconds/60*360) + 'deg)';
-    console.log(seconds);
+    console.log("Seconds: ", seconds);
     setTimeout(secRotation, 1000);
   }
   secRotation();
 
-// function annoy () {
-//     console.log("Are we there yet?")
-//     setTimeout(annoy, 1000)
-//   }
-//   const kids = setTimeout(annoy, 1000)
+  const minRotation = () => {
+    let now = new Date();
+    let minutes = now.getMinutes();
+    minute.style.transform = 'rotate(' + (minutes/60*360) + 'deg)';
+    console.log("Minutes: ", minutes);
+    setTimeout(minRotation, 1000);
+  }
+  minRotation();
 
-
-
-// On page load, start seconds timer and function that calculates how many degrees to turn
-// pass seconds to minute and hour functions that calculate how many degrees to turn the arm
-//secondRotation(0); // returns 0
-//secondRotation(59); // returns (59 / 60) * 360
-
-//hourRotation(12); // returns 0
-//hourRotation(5); // returns (5 / 12) * 360
+  const hourRotation = () => {
+    let now = new Date();
+    let hours = now.getHours();
+    hour.style.transform = 'rotate(' + (hours/12*360) + 'deg)';
+    console.log("Hours", hours);
+    setTimeout(hourRotation, 1000);
+  }
+  hourRotation();
