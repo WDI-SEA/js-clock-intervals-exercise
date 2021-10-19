b892c802d1090664c46c949bc8202398b04ccd78
iwaggoner <iwaggoner95@gmail.com>
2021-10-19 17:20:19 -0400

    modified clock.js so that hour hand is in right location, also made hour hand move inbetween each hour as well as minute hand moves inbetween each minute or every second

--- clock.js
+++ clock.js
@@ -5,10 +5,10 @@ const secondHand = document.getElementById('second')
 
 let now = new Date();
 var secondKeeper = (now.getSeconds() * 6)
-var minuteKeeper = (now.getMinutes() * 6)
-var hourKeeper = (now.getHours() * 24)
+var minuteKeeper = (now.getMinutes() * 6 + (now.getSeconds()*.1))
+var hourKeeper = (now.getHours() * 30 + (now.getMinutes()*.5))
 
-console.log(secondKeeper, minuteKeeper, hourKeeper)
+console.log(now)
 
 // secondHand.style.transform = 'rotate('+ secondKeeper +'deg)'
 minuteHand.style.transform = 'rotate(0deg)'
@@ -24,7 +24,6 @@ function startClock () {
 
 // second move function
 function secondMove() {
-    console.log("Second has passed")
     secondHand.style.transform = 'rotate('+ secondKeeper +'deg)'
     secondKeeper += 6
 }
@@ -32,20 +31,17 @@ function secondMove() {
   
 
 function minuteMove() {
-    console.log("Minute has passed")
     minuteHand.style.transform = 'rotate('+ minuteKeeper +'deg)'
-    minuteKeeper += 6
+    minuteKeeper += .1
 }
   
 
 function hourMove() {
-    console.log("Hour has passed?")
-    console.log("Minute has passed")
     hourHand.style.transform = 'rotate('+ hourKeeper +'deg)'
-    hourKeeper += 24
+    hourKeeper += .5
 }
   
 startClock()
 const secondRun = setInterval(secondMove, 1000)
-const minuteRun = setInterval(minuteMove, 60000)
-const hourRun = setInterval(hourMove, 3600000)
+const minuteRun = setInterval(minuteMove, 1000)
+const hourRun = setInterval(hourMove, 60000)
