e448c234d5a9a785b20787c205334b2349d50191
cmiltiadou <cmiltiad@gmail.com>
2021-10-18 21:18:17 -0400

    final update to clock.js for fully functioning clock

--- clock.js
+++ clock.js
@@ -7,11 +7,9 @@ let minuteHand = document.getElementById('minute')
 let secondHand = document.getElementById('second')
 
 let seconds = 0
-     
+let hours = 0
+let minutes = 0
 
-let secondRotation = (seconds) => {
-    return (seconds /60) * 360
-}
 
 let hourRotation = (hours) => {
     return (hours / 12) * 360
@@ -29,12 +27,29 @@ let minuteRotation = (minutes) => {
 
 // write function that will start counting up in seconds once page is loaded 
 
-const countup = () => {
+const countupSeconds = () => {
     seconds += 1;
-    return seconds
-    }
+    degreesSeconds = (seconds /60) * 360
+    secondHand.style.transform = "rotate(" + degreesSeconds +"deg)" 
+    } 
+const countupMinutes = () => {
+    minutes += 1;
+    degreesMinutes = (minutes /60) * 360
+    minuteHand.style.transform = "rotate(" + degreesMinutes +"deg)" 
+    } 
+const countupHours = () => {
+    hours += 1;
+    degreesHours =  (hours /60) * 360
+    hourHand.style.transform = "rotate(" + degreesHours +"deg)" 
+    } 
 
-setInterval(countup, 1000)
+setInterval(countupSeconds, 1000)
+setInterval(countupMinutes, 60010)
+setInterval(countupHours, 3600000)
+
+
+
+
 
 
 
