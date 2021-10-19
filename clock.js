61476f23ff7974bf56fdcefc8cfc40eb58e34877
DagemBefikadu <befikadu.dagem@gmail.com>
2021-10-18 21:05:53 -0700

    it works

--- clock.js
+++ clock.js
@@ -1,22 +1,51 @@
 //focus on each hand moving on its own interval
-//One section for each hand 
+//One section for each hand
 //each hand would involve its own set intervals
 
-//grab the elements that corrsepond with each hand 
+//grab the elements that corrsepond with each hand
 
-//Grabbing the DOM in the beginning 
+//Grabbing the DOM in the beginning
 
-let degrees = 45;
-let time = 90;
-let clock = 170
+// let secHand = 0;
+// let minHand = 0;
+// let hrHand = 0;
 
+const hourRotation = (arg) => {
+  return (arg / 12) * 360;
+};
+
+const minuteRotation = (arg) => {
+  return (arg / 60) * 360;
+};
+
+const secondRotation = (arg) => {
+  return (arg / 60) * 360;
+};
+
+const date = new Date();
 let sec = document.getElementById("second");
-sec.style.transform = "rotate(" + degrees + "deg)";
+const se = date.getSeconds();
+const degree = secondRotation(se);
+sec.style.transform = "rotate(" + degree + "deg)";
 
 let min = document.getElementById("minute");
-min.style.transform = "rotate(" + clock + "deg)";
+const mi = date.getMinutes();
+const minHand = minuteRotation(mi);
+min.style.transform = "rotate(" + minHand + "deg)";
 
-let hr = ocument.getElementById("hour");
-hr.style.transform = "rotate(" + time + "deg)";
+let hr = document.getElementById("hour");
+const hour = date.getHours();
+const hrHand = hourRotation(hour);
+hr.style.transform = "rotate(" + hrHand + "deg)";
 
+// function second() {
+//   if (secHand >= 60) {
+//     console.log(secHand);
+//     i++;
+//     setInterval(second, 1000);
+//   }
+// }
 
+setInterval(sec, 1000)
+setInterval(min, 1000)
+setInterval(hr, 1000)
