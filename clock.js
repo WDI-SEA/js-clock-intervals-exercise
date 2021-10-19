267a4f5ab8350f0ed21a626710fb8633a6d63e7b
jmeier2715 <91747651+jmeier2715@users.noreply.github.com>
2021-10-19 09:12:02 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -3,21 +3,21 @@ setInterval(function() {
   return(i++);
 }, 1000);
 
-// //function that rotates hour hand every 1 hour(3600000 millis) by 30 degrees an interval
+// //function that rotates hour hand
 let hourHand = document.getElementById('hour')
 function hourRotation() {
     return hourHand.style.transform = "rotate(" + ((1/120)*i) + "deg)"
 }
 const hourInterval = setInterval(hourRotation, 1000)
 
-// //function that rotates minute hand every 1 minute (60000 millis) by 6 degrees an interval
+// //function that rotates minute hand
 let minuteHand = document.getElementById('minute')
 function minuteRotation() {
     return minuteHand.style.transform = "rotate(" +(.1*i)+ "deg)"
 }
 const minuteInterval = setInterval(minuteRotation, 1000)
 
-// function that rotates second hand every 1 second (1000 millis) by 6 degrees an interval
+// function that rotates second hand
 let secondHand = document.getElementById('second')
 function secondRotation() {
     return secondHand.style.transform = "rotate(" + (6*i) + "deg)"
