306e9031fd053f240224eafcd964558492f60b04
shuzel99 <shuzellide@gmail.com>
2021-10-19 08:51:03 -0400

    big changes to js and clock complete

--- clock.js
+++ clock.js
@@ -1,17 +1,28 @@
-const hourHand = document.querySelector("#hour");
-const minuteHand = document.querySelector("#minute");
-const secondHand = document.querySelector("#second");
 
-const time = () => {
-    let hour = time.tellHour();
-    let minute = time.tellMinute();
-    let sec = time.tellSec();
-}
+// setInterval(setClock, 1000)
 
-const hourTick = hour*360/12 + ((minute * 360/60) / 12);
-const minuteTick = (minute * 360/60) + (sec* 360/60)/60;
-const secTick = sec * 360/60;
+var hourHand = document.getElementById('hour');
+var minuteHand = document.getElementById('minute');
+var secondHand = document.getElementById('second');
 
-hourHand.style.transform = "rotate(" + hourTick + "deg)";
-minuteHand.style.transform = "rotate (" + minuteTick + "deg)";
-secondHand.style.transform = "rotate (" + minuteTick + "deg)";
+function tellTime() {
+
+    var currentDate = new Date();
+    var hoursTick = currentDate.getHours() % 12;
+    var minutesTick = currentDate.getMinutes();
+    var secondsTick = currentDate.getSeconds();
+   
+    var hourDeg = (hoursTick * 30) + (0.5 * minutesTick);
+    var minuteDeg = (minutesTick * 6) + (0.1 * secondsTick);
+    var secondDeg = (secondsTick * 6);
+
+    hourHand.style.transform = 'rotate(' + hourDeg + 'deg)';
+    minuteHand.style.transform = 'rotate(' + minuteDeg + 'deg)';
+    secondHand.style.transform = 'rotate(' + secondDeg + 'deg)';
+
+   setTimeout(tellTime, 1000);
+
+};
+
+tellTime();
+
