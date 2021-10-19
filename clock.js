38c6a393520a83c0004107186a4b8714b0b927ba
paulinal3 <paulinal3@outlook.com>
2021-10-18 20:19:43 -0400

    second hand ticking

--- clock.js
+++ clock.js
@@ -2,5 +2,25 @@
 const hourHand = document.getElementById('hour')
 const minuteHand = document.getElementById('minute')
 const secondHand = document.getElementById('second')
+
 // create intervals to make the clock tick.
-// hour hand will run 
+// second hand will run 
+// will determine the new degree transformation
+let sDegree = 0
+const secDegrees = () => {
+    console.log(sDegree)
+    // sDegree += 6
+    return sDegree += 6
+    }
+
+// check to make sure increments of 6
+// console.log(secDegrees())
+// console.log(secDegrees())
+// console.log(secDegrees())
+
+// transform the second hand 6 degrees every second
+const seconds = () => {
+    secondHand.style.transform = `rotate(${secDegrees()}deg)`
+}
+
+const secondTick = setInterval(seconds, 1000)
