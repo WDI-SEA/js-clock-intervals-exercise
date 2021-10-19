17b32a53bbc0ebf0a995ff9d5d57bf2bf365fa6f
shuzel99 <shuzellide@gmail.com>
2021-10-19 09:18:50 -0400

    var to const

--- clock.js
+++ clock.js
@@ -1,20 +1,20 @@
 
 // setInterval(setClock, 1000)
 
-var hourHand = document.getElementById('hour');
-var minuteHand = document.getElementById('minute');
-var secondHand = document.getElementById('second');
+const hourHand = document.getElementById('hour');
+const minuteHand = document.getElementById('minute');
+const secondHand = document.getElementById('second');
 
 function tellTime() {
 
-    var currentDate = new Date();
-    var hoursTick = currentDate.getHours() % 12;
-    var minutesTick = currentDate.getMinutes();
-    var secondsTick = currentDate.getSeconds();
+    const currentDate = new Date();
+    const hoursTick = currentDate.getHours() % 12;
+    const minutesTick = currentDate.getMinutes();
+    const secondsTick = currentDate.getSeconds();
    
-    var hourDeg = (hoursTick * 30) + (0.5 * minutesTick);
-    var minuteDeg = (minutesTick * 6) + (0.1 * secondsTick);
-    var secondDeg = (secondsTick * 6);
+    const hourDeg = (hoursTick * 30) + (0.5 * minutesTick);
+    const minuteDeg = (minutesTick * 6) + (0.1 * secondsTick);
+    const secondDeg = (secondsTick * 6);
 
     hourHand.style.transform = 'rotate(' + hourDeg + 'deg)';
     minuteHand.style.transform = 'rotate(' + minuteDeg + 'deg)';
