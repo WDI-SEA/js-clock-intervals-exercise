d22720e59d2f0cf682414afa68092244f5db3249
Lawrencesalinas <lawrencesalinas@yahoo.com>
2021-12-29 03:43:45 -0500

    all hands are ticking rightfully

--- clock.js
+++ clock.js
@@ -1,24 +1,38 @@
 
 
 const secondHand = document.getElementById("second")
-const minutehand = document.getElementById('minute')
+const minuteHand = document.getElementById('minute')
 const hourHand  = document.getElementById("hour")
 
-let deg = 45;
-let el = document.getElementById("myElement");
-// el.style.transform = "rotate(" + degrees + "deg)";
-let sec = 0;
-
-const clockInt = () => {
-
-  rotate(secondHand, 1)
-  
- }
+let secondDegree = 0
+let minuteDegree = 0
+let hourDegree = 0
 
 
- const rotate = (el, deg) =>{
-    el.style.transform = `rotate(${deg}deg)`
-  }
-const rotateSecond = (secondHand, 50)
+const secondRotate = () => {
+  console.log('hello seconds', secondDegree += 1/60 * 360)
+  return secondHand.style.transform = `rotate(${secondDegree}deg)`
+}
 
-setInterval(clockInt, 1000)
+const minuteRotate = () => {
+  minuteDegree += 6/60
+  console.log('minute',minuteDegree);
+  return minuteHand.style.transform = `rotate(${minuteDegree}deg)`
+}
+
+const hourRotate = () => {
+  hourDegree += 1/120
+  console.log('hour',hourDegree);
+  return hourHand.style.transform = `rotate(${hourDegree}deg)`
+}
+
+const rotate = () => {
+  secondRotate()
+  minuteRotate()
+  hourRotate()
+}
+
+
+setInterval(rotate, 1000)
+
+
