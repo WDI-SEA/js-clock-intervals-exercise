8ee5c6cf0d4e39e820d3a538c197f85138add8ea
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-07 13:37:43 -0500

    formatted

--- clock.js
+++ clock.js
@@ -1,38 +1,32 @@
+const secondHand = document.getElementById("second");
+const minuteHand = document.getElementById("minute");
+const hourHand = document.getElementById("hour");
 
-
-const secondHand = document.getElementById("second")
-const minuteHand = document.getElementById('minute')
-const hourHand  = document.getElementById("hour")
-
-let secondDegree = 0
-let minuteDegree = 0
-let hourDegree = 0
-
+let secondDegree = 0;
+let minuteDegree = 0;
+let hourDegree = 0;
 
 const secondRotate = () => {
-  console.log('hello seconds', secondDegree += 1/60 * 360)
-  return secondHand.style.transform = `rotate(${secondDegree}deg)`
-}
+  console.log("hello seconds", (secondDegree += (1 / 60) * 360));
+  return (secondHand.style.transform = `rotate(${secondDegree}deg)`);
+};
 
 const minuteRotate = () => {
-  minuteDegree += 6/60
-  console.log('minute',minuteDegree);
-  return minuteHand.style.transform = `rotate(${minuteDegree}deg)`
-}
+  minuteDegree += 6 / 60;
+  console.log("minute", minuteDegree);
+  return (minuteHand.style.transform = `rotate(${minuteDegree}deg)`);
+};
 
 const hourRotate = () => {
-  hourDegree += 1/120
-  console.log('hour',hourDegree);
-  return hourHand.style.transform = `rotate(${hourDegree}deg)`
-}
+  hourDegree += 1 / 120;
+  console.log("hour", hourDegree);
+  return (hourHand.style.transform = `rotate(${hourDegree}deg)`);
+};
 
 const rotate = () => {
-  secondRotate()
-  minuteRotate()
-  hourRotate()
-}
+  secondRotate();
+  minuteRotate();
+  hourRotate();
+};
 
-
-setInterval(rotate, 1000)
-
-
+setInterval(rotate, 1000);
