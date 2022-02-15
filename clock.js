9068b30c838ecc6764f20f088fcb9ba76a604882
aoehmann239 <aoehmann239@gmail.com>
2022-02-15 00:45:12 -0500

    Included increasing degree variable inside function

--- clock.js
+++ clock.js
@@ -5,36 +5,42 @@
 //second hand-- 360 degrees every minute / 60 seconds= )1000ms  per 6 degrees
 //one tick on the Clock (360 degrees/60 ticks= 6 degrees per second)
 
-// const now = new Date()
-
-// let clockRotation = () => {
-
-// }
+// const timeNow = new Date()
+// let currentSeconds = timeNow.getSeconds()
+// let currentMinutes = timeNow.getMinutes()
+// let currentHour = timeNow.getHours()
 
 //the hour hand is moving 5/60 of the 360 degrees of the whole clock
 
-
+let degreeH = 30;
     let rotateHour = () =>{
+        
         let hourHand = document.getElementById("hour");
-        hourHand.style.transform = 'rotate(30deg)';
+        hourHand.style.transform = 'rotate('+ degreeH + 'deg)'
+        degreeH += 30
+
     // hourHand.rotate(30);
     }
 
 //the minute hand is moving 1/60th of 360
 
-
+let degreeM = 6
     let rotateMinute = () =>{
-        let minuteHand = document.getElementById("minute");
-        minuteHand.style.transform = 'rotate(6deg)';
-        // minuteHand.rotate(6);
         
+        let minuteHand = document.getElementById("minute");
+        minuteHand.style.transform = 'rotate('+ degreeM + 'deg)'
+        degreeM += 6
+        // minuteHand.rotate(6);  
     }
+
 //the second hand is also moving 1/60th of the 360 degrees clock, only faster
 
-
+let degreeS = 6
     let rotateSecond = () =>{
+        
         let secondHand = document.getElementById("second");
-        secondHand.style.transform = 'rotate(6deg)';
+        secondHand.style.transform = 'rotate('+ degreeS + 'deg)'
+        degreeS += 6
         // secondHand.rotate(6);
     }
 
