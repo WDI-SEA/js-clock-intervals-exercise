4463368d38430f13ebea7363636fb41a0307b027
poojad2101 <davepooja2101@gmail.com>
2022-05-20 03:02:17 +0530

    clock fixed

--- clock.js
+++ clock.js
@@ -14,9 +14,30 @@ let hours = 0;
 const runClock = () => {
     console.log();
 
-    const secondsRotation = setInterval(() => {
+    const secondsRotation= () => {let secondcounters = setInterval(() => {
         let degrees = (seconds / 60) * 360;
-        second.style.transform = "rotate(" + degrees + "deg)"
+        secondHand.style.transform = "rotate(" + degrees + "deg)"
         seconds++;
     }, 1000);
+    }
+
+    const minutesRotation= () => {let secondcounters = setInterval(() => {
+    let degrees = (seconds / 60) * 360;
+    secondHand.style.transform = "rotate(" + degrees + "deg)"
+    seconds++;
+}, 60000);
 }
+
+const hourRotation= () => {let secondcounters = setInterval(() => {
+    let degrees = (seconds / 60) * 360;
+    secondHand.style.transform = "rotate(" + degrees + "deg)"
+    seconds++;
+}, 1000);
+}
+
+secondsRotation();
+minutesRotation();
+hourRotation();
+}
+
+runClock();
