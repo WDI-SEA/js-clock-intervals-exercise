ace42083905a9fa8fcf8062f316f1ea2748a0fda
MramRashidR <mramrashid98@gmail.com>
2022-09-27 22:26:01 +0300

    not working still need modific

--- clock.js
+++ clock.js
@@ -1,3 +1,31 @@
 let Minute = document.querySelector('#minute');
 let Second = document.querySelector('#second');
-let Hour = document.querySelector('#hour')
+let Hour = document.querySelector('#hour');
+
+let degrees = 45;
+
+
+const TimeClc = () => {
+
+let secondRot = () => { 
+    return (Second / 60 *360)
+
+}
+
+let minRot = () => { 
+    return (Minute / 60 *360)
+
+}
+
+let hourRot = () => { 
+    return (Hour / 60 *360)
+
+}
+
+minRot.style.transform = "rotate(minRot)"
+secondRot.style.transform = "rotate(secondRot)"
+hourRot.style.transform = "rotate(hourRot)"
+
+}
+
+setInterval(TimeClc , 1000)
