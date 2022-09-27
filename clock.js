55e8064051cb7c11e574ec3fd170205e26893677
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-09-27 21:41:48 +0300

    Ahmed's Deliverbable

---
+++ clock.js
@@ -0,0 +1,49 @@
+
+
+const second = document.getElementById("second")
+const minute = document.getElementById("minute")
+const hour = document.getElementById("hour")
+
+
+//Defining the variables
+let secondsCount = 0, minutesCount = 0, hoursCount = 0, secondsDegree, minutesDegree, hoursDegree
+
+//Rotate the seconds
+const secondRotation = () => {
+    secondsDegree = (secondsCount/60)*360
+    second.style.transform = "rotate(" + secondsDegree + "deg)"
+
+    //Change minutes if it reaches 60 seconds
+    if(secondsCount%60 === 0){
+        setTimeout(minutesRotation, timeSpeed)
+
+    }
+    secondsCount++
+}
+
+
+//Rotate the minutes
+const minutesRotation = () => {
+    minutesDegree = (minutesCount/60)*360
+    minute.style.transform = "rotate(" + minutesDegree + "deg)"
+
+    //Change hour if it reaches 60 seconds
+    if(minutesCount%60 === 0){
+        setTimeout(hourRotation, timeSpeed)
+    }
+
+    minutesCount++
+}
+
+//Rotate the hours
+const hourRotation = () => {
+    hoursDegree = (hoursCount/12)*360
+    hour.style.transform = "rotate(" + hoursDegree + "deg)"
+    hoursCount++
+}
+
+
+//Start the clock!
+//Will call the secondRotation and the rest will follow
+let timeSpeed = 10
+setInterval(secondRotation, timeSpeed)
