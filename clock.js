05cc6949ae6a9f9e836ebde69c37d1f873fe80aa
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-09-28 10:18:30 +0300

    Done

--- clock.js
+++ clock.js
@@ -1,7 +1,7 @@
 
 const secondHand=document.getElementById('second')
 const minuteHand=document.getElementById('minute')
-const hourHand=document.getElementById('Hour')
+const hourHand=document.getElementById('hour')
 //initial varibles
 
 
@@ -16,12 +16,22 @@ setInterval(() => {
     let hour = date.getHours();
     let min= date.getMinutes();
     let second = date.getSeconds();
-    hr_rotation = 30 * hour + min / 2; 
+    hr_rotation = 30*hour; 
     min_rotation = 6 * min;
     sec_rotation = 6 * second;
-  
-    hourHand.style.transform = `rotate(${hr_rotation}deg)`;
+//   let secondTime=date.getSeconds()*6
+//   let minuteTime=date.getMinutes()*6
+//   let hourTime=date.getHours()*30
+
+//     hourHand.style.transform = `rotate(${hourTime}deg)`;
+    
+//     minuteHand.style.transform = `rotate(${minuteTime}deg)`;
+//     secondHand.style.transform = `rotate(${secondTime}deg)`;
+hourHand.style.transform = `rotate(${hr_rotation}deg)`;
+    
     minuteHand.style.transform = `rotate(${min_rotation}deg)`;
     secondHand.style.transform = `rotate(${sec_rotation}deg)`;
+
+
 }, 1000);
 
