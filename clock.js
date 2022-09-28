bd7eb04b8db3713426b97359ceac4a89ea805fdf
fjalafoo <fjalafoo@gmail.com>
2022-09-28 09:36:19 +0300

    added date now comments

--- clock.js
+++ clock.js
@@ -2,13 +2,21 @@ const secondPointer = document.getElementById("second")
 const minutePointer = document.getElementById("minute")
 const hourPointer = document.getElementById("hour")
 
-let secondCounter=0
-let secondDegree=0
 
+//const now = new Date()
+// let h=(now.getHours())-12
+// let min=now.getMinutes()
+// let sec=now.getSeconds()
+//console.log(h,min, sec)
+
+
+
+let secondCounter= 0
+let secondDegree=0
 
 const second = ()=>{
     secondPointer.style.transform=`rotate(${secondDegree+6}deg)`
-    console.log(secondCounter+1)
+    // console.log(secondCounter+1)
     secondCounter++
     secondDegree=secondDegree+6
 
@@ -24,7 +32,7 @@ let minuteDegree=0
 
 const minute = ()=>{
     minutePointer.style.transform=`rotate(${minuteDegree+6}deg)`
-    console.log(minuteCounter+1)
+    // console.log(minuteCounter+1)
     minuteCounter++
     minuteDegree=minuteDegree+6
 
@@ -40,7 +48,7 @@ let hourDegree=0
 
 const hour = ()=>{
     hourPointer.style.transform=`rotate(${hourDegree+6}deg)`
-    console.log(hourCounter+1)
+    // console.log(hourCounter+1)
     hourCounter++
     hourDegree=hourDegree+6
 
@@ -48,5 +56,4 @@ const hour = ()=>{
 
 
 const hours = setInterval(hour, 1000*60*5)
-
 
