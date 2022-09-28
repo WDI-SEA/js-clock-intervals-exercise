0226c6a05207823a06f7cc52d2d328e1acf5bb3f
Ameera Ali <amiraabdullamir@gmail.com>
2022-09-28 09:53:01 +0300

    bugs fixed

--- clock.js
+++ clock.js
@@ -13,6 +13,10 @@
   
 // }
 // }
+function initialPos(){
+  movehours();
+  moveMinutes();
+}
 function moveSeconds(){
     let degInput = 0
    let secondsHand=0;
@@ -23,22 +27,23 @@ function moveSeconds(){
 }  
 function movehours(){
     let degInput = 0
-   let hoursHand=0;
       let hours=new Date().getHours()
-    hoursHand = hours/5;
-  degInput = hoursHand*30
+    
+  degInput = hours*30
    document.getElementById("hour").style.transform = `rotate(${degInput}deg)`
+   console.log(degInput)
 }  
 function moveMinutes(){
     let degInput = 0
    let minutesHand=0;
       let minutes=new Date().getMinutes()
-    minutesHand = seconds/5;
+    minutesHand = minutes/5;
   degInput = minutesHand*30
    document.getElementById("minute").style.transform = `rotate(${degInput}deg)`
+   console.log(degInput)
 }  
  
- 
+ initialPos();
 setInterval(moveSeconds,1000)//every second
 setInterval(moveMinutes,60000)//every 60 seconds
 setInterval(movehours,3600000)//every 60 minutes
