231e4b02673e3c38330ec85fd46c0e1f83a109d9
Ameera Ali <amiraabdullamir@gmail.com>
2022-09-28 08:44:02 +0300

    functions added - ameera ali

---
+++ clock.js
@@ -0,0 +1,44 @@
+
+//      let seconds=15
+
+// function moveSeconds(){
+//   let degInput = 0
+//  let secondsHand=0;
+
+//   secondsHand = seconds/5;
+// degInput = secondsHand*30
+//  document.getElementById("second").style.transform = `rotate(${degInput}deg)`
+//  seconds = seconds+1
+// console.log(seconds)
+  
+// }
+// }
+function moveSeconds(){
+    let degInput = 0
+   let secondsHand=0;
+      let seconds=new Date().getSeconds()
+    secondsHand = seconds/5;
+  degInput = secondsHand*30
+   document.getElementById("second").style.transform = `rotate(${degInput}deg)`
+}  
+function movehours(){
+    let degInput = 0
+   let hoursHand=0;
+      let hours=new Date().getHours()
+    hoursHand = hours/5;
+  degInput = hoursHand*30
+   document.getElementById("hour").style.transform = `rotate(${degInput}deg)`
+}  
+function moveMinutes(){
+    let degInput = 0
+   let minutesHand=0;
+      let minutes=new Date().getMinutes()
+    minutesHand = seconds/5;
+  degInput = minutesHand*30
+   document.getElementById("minute").style.transform = `rotate(${degInput}deg)`
+}  
+ 
+ 
+setInterval(moveSeconds,1000)//every second
+setInterval(moveMinutes,60000)//every 60 seconds
+setInterval(movehours,3600000)//every 60 minutes
