cfc2fcd249a446e73b72890a9a82b6c62d354b66
user <ranyamahmood55@gmail.com>
2022-11-27 22:04:26 +0300

    new solution

--- clock.js
+++ clock.js
@@ -1,50 +1,43 @@
-const secondPointer = document.getElementById("second")
-const minutePointer = document.getElementById("minute")
-const hourPointer = document.getElementById("hour")
+const secondPointer = document.getElementById("second");
+const minutePointer = document.getElementById("minute");
+const hourPointer = document.getElementById("hour");
 
-let secondCounter=0
-let secondDegree=0
+// let secondCounter
+// let secondDegree
 
 
-const second = ()=>{
-    secondPointer.style.transform=`rotate(${secondDegree+6}deg)`
-    console.log(secondCounter+1)
-    secondCounter++
-    secondDegree=secondDegree+6
+Function setTime() {
+    const now = new Date ();
+
+
+
+const seconds = new.getSeconds();
+const secondsDegree = ((seconds / 60)  *360) +90;
+secondsElement.style.transfrom = 'rotate(${secondsDegrees}deg';
+
+const mins = now.getMinutes();
+const secondsDegree = ((main / 60 ) * 360) + ((seconds/60)*6) + 90;
+SVGAnimateMotionElement.style.teansfrom = 'rotate(${minDegrees}deg';
+
+
+
+const hour = now.getHours();
+console.log(hour)
+const hourDegree = ((hour /12) *360) + ((mins/60)*30)+ 90;
+console.log(hourDegree)
+HTMLSourceElement.style.transfrom = 'rotate(${hourDegrees}deg';
+
 
 }
 
 
-const seconds = setInterval(second, 1000)
+setInterval(setTime, 1000);
+
+setTime()
+// const minutes = setInterval(minute, 1000*60)
 
 
-let minuteCounter=0
-let minuteDegree=0
+// let hourCounter=0
+// let hourDegree=0
 
 
-const minute = ()=>{
-    minutePointer.style.transform=`rotate(${minuteDegree+6}deg)`
-    console.log(minuteCounter+1)
-    minuteCounter++
-    minuteDegree=minuteDegree+6
-
-}
-
-
-const minutes = setInterval(minute, 1000*60)
-
-
-let hourCounter=0
-let hourDegree=0
-
-
-const hour = ()=>{
-    hourPointer.style.transform=`rotate(${hourDegree+6}deg)`
-    console.log(hourCounter+1)
-    hourCounter++
-    hourDegree=hourDegree+6
-
-}
-
-
-const hours = setInterval(hour, 1000*60*5)
