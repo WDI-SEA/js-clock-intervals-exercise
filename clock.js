1e98bef90ca1353b7998b7d8e9e87d581c17d96a
rectorjordan94 <rectorjordan94@gmail.com>
2022-12-19 14:56:59 -0600

    updated styles, html, and logic to have a fully functioning clock using the real time

---
+++ clock.js
@@ -0,0 +1,59 @@
+const minuteHand = document.getElementById('minute')
+const secondHand = document.getElementById('second')
+const hourHand = document.getElementById('hour')
+
+const secondRotation = (second) => {
+    return (second / 60) * 360
+}
+
+const minuteRotation = (minute) => {
+    return (minute / 60) * 360
+}
+
+const hourRotation = (hour) => {
+    return (hour / 12) * 360
+}
+
+const getCurrentTime = () => {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const initialSecRotate = secondRotation(seconds)
+    secondHand.style.transform = `rotate(${initialSecRotate}deg)`
+    const initialMinRotate = minuteRotation(minutes)
+    minuteHand.style.transform = `rotate(${initialMinRotate}deg)`
+    const initialHourRotate = hourRotation(hours)
+    hourHand.style.transform = `rotate(${initialHourRotate}deg)`
+    return [seconds, minutes, hours]
+}
+
+const rotateSecondHand = () => {
+    let seconds = getCurrentTime()[0]
+    let degrees = secondRotation(seconds)
+    secondHand.style.transform = `rotate(${degrees}deg)`
+}
+
+const rotateMinuteHand = () => {
+    let minutes = getCurrentTime()[1]
+    let degrees = minuteRotation(minutes)
+    minuteHand.style.transform = `rotate(${degrees}deg)`
+    console.log(minutes)
+}
+
+const rotateHourHand = () => {
+    let hours = getCurrentTime()[2]
+    let degrees = minuteRotation(hours)
+    hourHand.style.transform = `rotate(${degrees}deg)`
+}
+
+const seconds = setInterval(rotateSecondHand, 1000)
+
+const minutes = setInterval(rotateMinuteHand, 60000)
+
+const hours = setInterval(rotateHourHand, 3.6e+6)
+
+
+// document.addEventListener('DOMContentLoaded', getCurrentTime)
+
+
